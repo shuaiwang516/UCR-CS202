a52446227c5d1e618d5e397f242058fea475f4e3
Shuai Wang <swang378@sledge.cs.ucr.edu>
2020-06-03 23:12:46 -0700

    final version

--- frisbee.c
+++ frisbee.c
@@ -2,74 +2,48 @@
 #include "stat.h"
 #include "user.h"
 #include "thread.h"
-#define DEBUG 0
-struct lock_t *lock;
-static int output =0;
-static int numofthread;
-static int passnum;
-static int workpid=0;
 
-void* worker();
-int main ( int argc , char * argv [])
-{
-	numofthread =atoi(argv[1]);
-	passnum = atoi(argv[2]);
-	int i = 0 ;
-	lock_init(lock);
-	for(i=0;i<numofthread;i++)
-	{
-		thread_create(worker,(void *)i);
-	}
-	for(i=0;i<numofthread;i++)
-	{
-		wait();
-        //        printf(1,"wait = %d\n",i);
-	}
-        //printf(1,"wait finish\n");
-	exit();
-#if DEBUG
-	if(pid >0)
-		printf(1,"parent's pid num is %d\n",getpid());
-	if(pid ==0)
-		printf(1,"child's pid num is %d\n",getpid());
-#endif
+struct lock_t *spinLock;
+static int totalNumber;
+static int passNumber = 0;
+static int threadNumber;
+static int passTo = 0;
 
-
-#if DEBUD
-	printf(1,"lock_test = %d\n",lock->locked);
-#endif 
-
+void* threadFunction(void* arg){
+    int tid = (int)arg;
+    while(passNumber < totalNumber){
+        lock_acquire(spinLock);
+        if(tid == passTo){
+            passTo = tid + 1 >= threadNumber ? 0 : tid + 1;
+            printf(1, "Pass Number no: %d, Thread %d is passing the token to thread %d\n", ++passNumber, tid, passTo);
+        }
+        lock_release(spinLock);
+        sleep(10);
+    }
+    exit();
 }
-void* worker(void *arg)
-{
-	int pidnum = (int)arg;
-#if DEBUG
-	printf(1,"child's pid num is %d\n",pidnum);
-#endif
-	while(output<passnum)
-	{
-		lock_acquire(lock);
-		if(output==passnum)
-			break;
-		if(pidnum==workpid)
-		{
-			output++;
-			printf(1,"pass number no:%d is thread %d is passing the token to ",output,pidnum);
-			workpid++;
-			if(workpid ==  numofthread)// because thread num is start from 0 
-				workpid = 0 ;	
-			printf(1," %d\n",workpid);
-			lock_release(lock);
-		 	sleep(1);
-		}
-		else
-		{
-			lock_release(lock);
-			sleep(1);
-		}
-	}
-	
-	//printf(1,"time to end\n");
-	exit();
-	
+
+int main(int argc, char* argv[]){
+    if(argc != 3){
+        printf(1, "Frisbee needs 2 extra elements!!\n");
+    }
+    int i = 0;
+    int tid[4] = {0, 1, 2, 3};
+    threadNumber = atoi(argv[1]);
+    totalNumber = atoi(argv[2]);
+
+    lock_init(spinLock);
+
+    for(i = 0; i < threadNumber; i++){
+        //Using a array to give the threadID to each threads
+        //in order to prevent race condition.
+        //printf(1,"currently giving %d\n", tid[i]);
+        thread_create(threadFunction, (void*)tid[i]);
+    }
+
+    for(i = 0; i < threadNumber; i++){
+        wait();
+    }
+
+    exit();
 }
